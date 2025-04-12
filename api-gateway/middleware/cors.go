17f78c03609c99769013e430319262eabfab0257
kevinnaserwan <kevinnaserwan@gmail.com>
2025-04-12 13:02:16 +0700

    feat: adjust x-api-key

--- api-gateway/middleware/cors.go
+++ api-gateway/middleware/cors.go
@@ -10,7 +10,7 @@ func CORS() gin.HandlerFunc {
 	return cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "x-api-key"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "x-api-key", "X-Auth-Timestamp", "X-Auth-Signature"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 	})
